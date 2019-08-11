8334764c2d34c9862acf75e5c1c87bea21c0ff0c
tuychin <tuychin@gmail.com>
2019-08-12 02:14:44 +0300

    Header refactored

--- src/components/sort/sort.js
+++ src/components/sort/sort.js
@@ -4,16 +4,19 @@ import './sort.css';
 
 const Sort = () => {
   return (
-    <div className="sort btn-group btn-group-toggle" data-toggle="buttons">
-      <label className="btn btn-primary active">
-        <input type="radio" name="options" id="option1" autoComplete="off" defaultChecked="" /> Новые
-      </label>
-      <label className="btn btn-primary">
-        <input type="radio" name="options" id="option2" autoComplete="off" /> Популярные
-      </label>
-      <label className="btn btn-primary">
-        <input type="radio" name="options" id="option3" autoComplete="off" /> По возрастанию цены
-      </label>
+    <div className="sort">
+      <span className="sort-text sort__sort-text">СОРТИРОВАТЬ:</span>
+      <div className="sort btn-group btn-group-toggle" data-toggle="buttons">
+        <label className="btn btn-primary active">
+          <input type="radio" name="options" id="option1" autoComplete="off" defaultChecked="" /> Новые
+        </label>
+        <label className="btn btn-primary">
+          <input type="radio" name="options" id="option2" autoComplete="off" /> Популярные
+        </label>
+        <label className="btn btn-primary">
+          <input type="radio" name="options" id="option3" autoComplete="off" /> По цене
+        </label>
+      </div>
     </div>
   );
 }
